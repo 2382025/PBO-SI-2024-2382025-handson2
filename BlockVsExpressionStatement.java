d67b3bbd0843af70c131980a796a7ac871578a2c
2382025 <2382025@unai.edu>
2024-09-10 10:27:29 +0700

    handson2

---
+++ BlockVsExpressionStatement.java
@@ -0,0 +1,13 @@
+public class BlockVsExpressionStatement {
+    public static void main(String[] args) {
+
+        //Ekpresi -> 10 + 10
+        //Ekspresi a = 20;
+        //Statement -> int a = 10 + 10;
+        int a = 10 + 10;
+        System.out.println("Grace Eunice Aritonang");
+        System.out.println("Grace Eunice Aritonang");
+        System.out.println("Grace Eunice Aritonang");
+        System.out.println("Grace Eunice Aritonang");
+    }
+}
---
+++ Soal1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Soal1 {
+        /**
+         * Buatlah sebuah program yang mengambil 2 input number dari user
+         * program tsb akan print out nilai yang paling besar
+   **/
+        public static void main(String[] args) {
+
+    }
+}
